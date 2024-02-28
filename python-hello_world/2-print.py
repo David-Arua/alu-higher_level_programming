474c9fd9c7f49da085c20417b0c36c7ffd3d88d3
David-Arua <silverlightacrh@gmail.com>
2024-02-28 10:33:08 +0200

    working on my assignments

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+storage = "\"Programming is like building a multilingual puzzle\n"
+print(storage)
---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+manipulator = f"{3 * str}\n{str[0:9]}"
+print(manipulator)
