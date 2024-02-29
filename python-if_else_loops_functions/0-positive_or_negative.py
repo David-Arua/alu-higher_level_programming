ed65b9d600b58cdada0c38dc84b4763bdffa3e41
David-Arua <silverlightacrh@gmail.com>
2024-02-29 13:48:11 +0200

    fix typo

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:
-    print(number, 'is negtive')
+    print(number, 'is negative')
 elif number == 0:
     print(number, 'is zero')
 else:
