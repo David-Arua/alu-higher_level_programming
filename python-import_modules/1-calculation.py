79e7da1f09e0887800a12a9795d0591c2135af2e
David-Arua <silverlightacrh@gmail.com>
2024-03-01 17:22:43 +0200

    fixed blank lines

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 1-calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
@@ -13,4 +10,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-    
