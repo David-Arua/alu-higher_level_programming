7fa095ef582a327af4c540a7fd00da417ab983c4
David-Arua <silverlightacrh@gmail.com>
2024-03-24 18:11:10 +0200

    fix

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id
+--  lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title;
