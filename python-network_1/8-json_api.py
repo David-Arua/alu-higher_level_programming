ddcb3f2b1923ecd6b11fa63e4eb194f10a3a1df1
David-Arua <silverlightacrh@gmail.com>
2024-03-24 19:30:35 +0200

    fix

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that takes in a URL,
-- sends a request to the URL and
-- displays the body of the response.
-"""
-import sys
+"""Search API"""
 import requests
+import sys
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-
-    reqs = requests.get(url)
-    if reqs.status_code >= 400:
-        print('Error code: {}'.format(reqs.status_code))
-    else:
-        print(reqs.text)
+    try:
+        params = sys.argv[1]
+    except IndexError:
+        params = ""
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user",
+        data={"q": params}
+    )
+    try:
+        json_response = response.json()
+        if response.headers.get("Content-Type") == 'application/json':
+            if len(json_response) > 0:
+                print("[{}] {}".format(
+                    json_response["id"],
+                    json_response["name"])
+                )
+            else:
+                print("No result")
+    except:
+        print("Not a valid JSON")
