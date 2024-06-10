cddc0a759119568442996d36136f3c80919a5270
David Arua <brute@Davids-Laptop.local>
2024-06-10 15:30:25 +0200

    done

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+    let num = 0;
+    list.forEach(element => {
+      if (element === searchElement) {
+        num += 1;
+      }
+    });
+    return num;
+  };
+  
