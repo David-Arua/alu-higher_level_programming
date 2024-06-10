c2d81ac86196060a1d6e0aa154098b3f58e75294
David Arua <brute@Davids-Laptop.local>
2024-06-10 16:08:22 +0200

    done

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$.ajax({
+    url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+    type: 'GET',
+    success: (res) => {
+      $('#character').text(res.name);
+    }
+  });
+
